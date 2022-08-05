bd93b33fa32fe61372d40008a2864a33a678f539
unknown <1456065801@qq.com>
2022-08-05 10:46:13 +0800

    dfsaf

---
+++ src/main/java/a/TT.java
@@ -0,0 +1,12 @@
+package a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TT {
+    public static void main(String[] args) {
+        SpringApplication.run(TT.class,args);
+        System.out.println(333);
+    }
+}
